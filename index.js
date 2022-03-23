eb1e12a325fae46a0bb32a6a3918e4667d5b9fa1
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-03-23 16:48:05 +0100

    prepare exercice

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const valueEuro = document.getElementById("valueEuro");
 const result = document.getElementById("result");
 const buttonChange = document.getElementById("convert-btn");
 const currency = document.getElementById("currency");
-const newResult = document.getElementById("new-result");
+
 const rateInput = document.getElementById("new-change");
-const newCurrency = document.getElementById("new-currency");
+
 const rateDefault = document.getElementById("default-rate");
 
 const change = 131.62;
@@ -16,12 +16,4 @@ valueEuro.addEventListener("input", (e) => {
   const changeCalculate = (valueEuro.value * change).toFixed([2]);
   result.innerText = changeCalculate;
   currency.innerText = "YEN";
-});
-
-buttonChange.addEventListener("click", (e) => {
-  e.preventDefault();
-  const newChangeCalculate = (valueEuro.value * rateInput.value).toFixed([2]);
-  newResult.innerText = newChangeCalculate;
-  console.log(newChangeCalculate);
-  newCurrency.innerText = "YEN";
 });
